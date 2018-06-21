e914efd2ccf36ad31d17b44b79649159c3cc664f
ejpark <ejpark78@gmail.com>
2018-06-21 10:09:56 +0900

    insert_question_list 모듈 삭제

--- naver_kin_crawler.py
+++ naver_kin_crawler.py
@@ -110,6 +110,66 @@ class NaverKinCrawler(Utils):
             }
         ]
 
+        self.category_list = {
+            '금융': '401',
+            '예금, 적금': '40101',
+            '주식, 증권': '40102',
+            '보험': '40103',
+            '신용카드': '40104',
+            '대출': '40105',
+            '개인 신용, 회복': '40106',
+            '뱅킹, 금융업무': '40107',
+            '가상화폐': '40108',
+            '부동산': '402',
+            '매매': '40201',
+            '임대차': '40202',
+            '분양, 청약': '40203',
+            '경매': '40204',
+            '세금, 세무': '403',
+            '소득세': '40301',
+            '법인세': '40302',
+            '상속세, 증여세': '40303',
+            '부가가치세': '40304',
+            '종합부동산세': '40305',
+            '취득세, 등록세': '40306',
+            '재산세': '40307',
+            '자동차세': '40308',
+            '세금 정책, 제도': '40309',
+            '관세': '40310',
+            '연말정산': '40311',
+            '경영': '404',
+            '마케팅, 영업': '40401',
+            '회계, 감사, 재무 관리': '40402',
+            '인사, 조직 관리': '40403',
+            '연구개발, 생산, 물류': '40404',
+            '경영정보시스템': '40405',
+            '무역': '405',
+            '수출': '40501',
+            '수입': '40502',
+            '직업, 취업': '406',
+            '공무원': '40601',
+            '교육, 연구, 학문': '40602',
+            '의료, 법률, 금융': '40603',
+            '복지, 종교': '40604',
+            '문화, 예술': '40605',
+            '관광, 요식업, 스포츠': '40606',
+            '유통, 판매, 영업': '40607',
+            '컴퓨터, 프로그래밍': '40608',
+            '생산, 기술': '40609',
+            '사무지원': '40610',
+            '아르바이트': '40611',
+            '취업 정책, 제도': '40612',
+            '구인구직사이트': '40613',
+            '창업': '407',
+            '판매, 유통업': '40701',
+            '관광, 요식업': '40702',
+            '서비스업': '40703',
+            '창업성장': '40704',
+            '경제 정책, 제도': '408',
+            '경제 동향, 이론': '409',
+            '경제 기관, 단체': '410',
+        }
+
     @staticmethod
     def set_pragma(cursor, readonly=True):
         """
@@ -203,7 +263,7 @@ class NaverKinCrawler(Utils):
         return False
 
     @staticmethod
-    def save_content(cursor, table_name, values, commit=True, conn=None):
+    def save_content(cursor, table_name, values, update=False, commit=True, conn=None):
         """
         입력 받은 html 저장
 
@@ -212,6 +272,7 @@ class NaverKinCrawler(Utils):
         :param table_name: 테이블명
         :param values: 저장 값 (d1_id, dir_id, doc_id, content,)
         :param commit: 커밋 여부
+        :param update: 업데이트 여부
         :return:
         """
         if table_name == 'user_list':
@@ -221,26 +282,102 @@ class NaverKinCrawler(Utils):
 
         try:
             cursor.execute(sql, values)
+        except sqlite3.IntegrityError as e:
+            if update is True:
+                if table_name == 'user_list':
+                    pass
+                else:
+                    update_values = (values[3], values[0], values[1], values[2],)
 
-            if conn is not None and commit is True:
-                conn.commit()
+                    sql = 'UPDATE {} SET content=? WHERE d1_id=? AND dir_id=? AND doc_id=?'.format(table_name)
+                    try:
+                        cursor.execute(sql, update_values)
+                        logging.info('디비 내용 업데이트: {}-{}'.format(values[1], values[2]))
+                    except Exception as e:
+                        logging.error('디비 업데이트 오류: {}'.format(e))
+            else:
+                logging.error('키 중복: {}'.format(e))
         except Exception as e:
             logging.error('디비 저장 오류: {}'.format(e))
 
+        try:
+            if conn is not None and commit is True:
+                conn.commit()
+        except Exception as e:
+            logging.error('디비 커밋 오류: {}'.format(e))
+
         return
 
-    def batch_question_list(self):
+    def batch_question_list(self, result_path):
         """
         질문 목록 전부를 가져온다.
+        :param result_path: 결과 저장 경로
+        :return:
         """
         url = 'https://m.kin.naver.com/mobile/qna/kinupList.nhn?' \
               'sortType=writeTime&resultMode=json&dirId={dir_id}&countPerPage={size}&page={page}'
 
         dir_id_list = [4, 11, 1, 2, 3, 8, 7, 6, 9, 10, 5, 13, 12, 20]
         for dir_id in dir_id_list:
-            result_path = '{data_path}/{dir_id}'.format(data_path=self.data_home, dir_id=dir_id)
+            self.get_question_list(url=url, dir_id=dir_id, result_path=result_path, end=500)
 
-            self.get_question_list(url=url, dir_id=dir_id, result_path=result_path, end=20)
+        return
+
+    def get_question_list(self, url, dir_id=4, start=1, end=90000, size=20,
+                          result_path='data/naver/kin/question_list'):
+        """
+        네이버 지식인 경제 분야 크롤링
+
+        :param url: 쿼리 조회 URL 패턴
+            (https://m.kin.naver.com/mobile/qna/kinupList.nhn?
+                sortType=writeTime&resultMode=json&dirId={dir_id}&countPerPage={size}&page={page})
+        :param dir_id: 도메인 (4: 경제)
+        :param start: 시작 페이지
+        :param end: 종료 페이지
+        :param size: 페이지 크기
+        :param result_path: 결과를 저장할 경로
+        :return:
+        """
+        import requests
+        from time import sleep
+
+        delay = 5
+
+        for page in range(start, end):
+            query_url = url.format(dir_id=dir_id, size=size, page=page)
+
+            request_result = requests.get(url=query_url, headers=self.headers,
+                                          allow_redirects=True, timeout=60)
+
+            result = request_result.json()
+
+            result_list = []
+            if 'answerList' in result:
+                result_list = result['answerList']
+
+            if 'lists' in result:
+                result_list = result['lists']
+
+            if len(result_list) == 0:
+                break
+
+            logging.info(msg='{} {:,} ~ {:,} {:,}'.format(dir_id, page, end, len(result_list)))
+
+            # 결과 저장
+            question_list = {}
+
+            # 카테고리별로 분리
+            for q in result_list:
+                dir_id = q['dirId']
+
+                if dir_id not in question_list:
+                    question_list[dir_id] = []
+
+                question_list[dir_id].append(q)
+
+            self.save_question_list(question_list=question_list, result_path=result_path, update=True)
+
+            sleep(delay)
 
         return
 
@@ -279,63 +416,6 @@ class NaverKinCrawler(Utils):
             target_path = '{data_path}/{user_name}'.format(data_path=result_path, user_name=user_name)
 
             self.get_question_list(url=query_url, dir_id=0, result_path=target_path)
-
-        return
-
-    def get_question_list(self, url, dir_id=4, start=1, end=90000, size=20,
-                          result_path='data/naver/kin'):
-        """
-        네이버 지식인 경제 분야 크롤링
-
-        :param url: 쿼리 조회 URL 패턴
-            (https://m.kin.naver.com/mobile/qna/kinupList.nhn?
-                sortType=writeTime&resultMode=json&dirId={dir_id}&countPerPage={size}&page={page})
-        :param dir_id: 도메인 (4: 경제)
-        :param start: 시작 페이지
-        :param end: 종료 페이지
-        :param size: 페이지 크기
-        :param result_path: 결과를 저장할 경로
-        :return:
-        """
-        import json
-        import requests
-        from time import sleep
-
-        delay = 5
-
-        if not os.path.isdir(result_path):
-            os.makedirs(result_path)
-
-        for page in range(start, end):
-            file_name = '{result_path}/{page:04d}.json'.format(result_path=result_path, page=page)
-
-            if os.path.exists(file_name):
-                logging.info(msg='skip {}'.format(page))
-                continue
-
-            query_url = url.format(dir_id=dir_id, size=size, page=page)
-
-            request_result = requests.get(url=query_url, headers=self.headers,
-                                          allow_redirects=True, timeout=60)
-
-            result = request_result.json()
-
-            result_list = []
-            if 'answerList' in result:
-                result_list = result['answerList']
-
-            if 'lists' in result:
-                result_list = result['lists']
-
-            if len(result_list) == 0:
-                break
-
-            logging.info(msg='{} {:,} {:,}'.format(file_name, page, len(result_list)))
-            with open(file_name, 'w') as fp:
-                str_lists = json.dumps(result_list, indent=4, ensure_ascii=False)
-                fp.write(str_lists)
-
-            sleep(delay)
 
         return
 
@@ -405,74 +485,14 @@ class NaverKinCrawler(Utils):
                           'Version/11.0 Mobile/15A372 Safari/604.1'
         }
 
-        category_list = {
-            '금융': '401',
-            '예금, 적금': '40101',
-            '주식, 증권': '40102',
-            '보험': '40103',
-            '신용카드': '40104',
-            '대출': '40105',
-            '개인 신용, 회복': '40106',
-            '뱅킹, 금융업무': '40107',
-            '가상화폐': '40108',
-            '부동산': '402',
-            '매매': '40201',
-            '임대차': '40202',
-            '분양, 청약': '40203',
-            '경매': '40204',
-            '세금, 세무': '403',
-            '소득세': '40301',
-            '법인세': '40302',
-            '상속세, 증여세': '40303',
-            '부가가치세': '40304',
-            '종합부동산세': '40305',
-            '취득세, 등록세': '40306',
-            '재산세': '40307',
-            '자동차세': '40308',
-            '세금 정책, 제도': '40309',
-            '관세': '40310',
-            '연말정산': '40311',
-            '경영': '404',
-            '마케팅, 영업': '40401',
-            '회계, 감사, 재무 관리': '40402',
-            '인사, 조직 관리': '40403',
-            '연구개발, 생산, 물류': '40404',
-            '경영정보시스템': '40405',
-            '무역': '405',
-            '수출': '40501',
-            '수입': '40502',
-            '직업, 취업': '406',
-            '공무원': '40601',
-            '교육, 연구, 학문': '40602',
-            '의료, 법률, 금융': '40603',
-            '복지, 종교': '40604',
-            '문화, 예술': '40605',
-            '관광, 요식업, 스포츠': '40606',
-            '유통, 판매, 영업': '40607',
-            '컴퓨터, 프로그래밍': '40608',
-            '생산, 기술': '40609',
-            '사무지원': '40610',
-            '아르바이트': '40611',
-            '취업 정책, 제도': '40612',
-            '구인구직사이트': '40613',
-            '창업': '407',
-            '판매, 유통업': '40701',
-            '관광, 요식업': '40702',
-            '서비스업': '40703',
-            '창업성장': '40704',
-            '경제 정책, 제도': '408',
-            '경제 동향, 이론': '409',
-            '경제 기관, 단체': '410',
-        }
-
         url_frame = 'https://kin.naver.com/qna/directoryExpertList.nhn?dirId={dir_id}'
         list_url_frame = [
             'https://kin.naver.com/ajax/qnaWeekRankAjax.nhn?requestId=weekRank&dirId={dir_id}&page={page}',
             'https://kin.naver.com/ajax/qnaTotalRankAjax.nhn?requestId=totalRank&dirId={dir_id}&page={page}'
         ]
 
-        for dir_name in category_list:
-            dir_id = category_list[dir_name]
+        for dir_name in self.category_list:
+            dir_id = self.category_list[dir_name]
             url = url_frame.format(dir_id=dir_id)
 
             result = requests.get(url=url, headers=headers,
@@ -655,16 +675,17 @@ class NaverKinCrawler(Utils):
 
         return
 
-    def batch_save_content(self, cursor, table_name, value_list):
+    def batch_save_content(self, cursor, table_name, value_list, update=False):
         """
         답변 상세 페이지를 10개 단위로 배치 저장
         :param cursor: DB 커서
         :param table_name: 테이블명
         :param value_list: 저장할 데이터
+        :param update: 데이터 갱신 여부
         :return:
         """
         for values in value_list:
-            self.save_content(cursor=cursor, table_name=table_name,
+            self.save_content(cursor=cursor, table_name=table_name, update=update,
                               values=values, commit=False, conn=None)
 
         return
@@ -918,7 +939,8 @@ class NaverKinCrawler(Utils):
                 "honorKin": false,
                 "month": 5,
                 "powerKin": false,
-                "profilePhotoUrl": "https://kin-phinf.pstatic.net/20180426_110/1524732986074Gv6GP_JPEG/%BA%CE%C7%B0%C1%A4%B8%AE.jpg?type=w200",
+                "profilePhotoUrl": "https://kin-phinf.pstatic.net/20180426_110/1524732986074Gv6GP_JPEG/
+                    %BA%CE%C7%B0%C1%A4%B8%AE.jpg?type=w200",
                 "selectBestCnt": 2997,
                 "u": "X/XrUkwNpssPPgqPB+TNUGm+PjMq/YHPNL4nAHhrM74=",
                 "viewId": "박학다식전문가",
@@ -1019,102 +1041,23 @@ class NaverKinCrawler(Utils):
 
         return
 
-    def insert_question_list(self, data_path=''):
+    def save_question_list(self, question_list, result_path, update=False):
         """
-        질문 목록을 sqlite3 에 입력
-        :param data_path: 사용자 목록이 저장된 경로 data/naver/kin/question_list/경제
-
-        질문 자료 구조
-            {
-                "thumbnailInfos": null,
-                "betPoint": 0,
-                "kinupPoint": 0,
-                "d2id": 701,
-                "readCnt": 1,
-                "isContainsLocation": false,
-                "d1Id": 7,
-                "title": "명치아플땐?",
-                "qboardId": 0,
-                "answerCnt": 1,
-                "writeTime": 1529107180000,
-                "isFromMobile": true,
-                "firstFlag": null,
-                "mediaFlag": 0,
-                "isAutoTitle": true,
-                "kinupCnt": 0,
-                "isAdult": false,
-                "fullDirNamePath": "Q&A > 건강 > 건강상담 > 내과 > 소화기내과",
-                "autoTitle": false,
-                "renewFlag": null,
-                "gdId": "10000009_00001218bc79",
-                "tags": null,
-                "d5id": 0,
-                "unreadFlag": null,
-                "entryLink": null,
-                "answerTime": null,
-                "adultFlag": "N",
-                "inputDevice": "MOBILE_WEB",
-                "dirName": "소화기내과",
-                "kinFlag": null,
-                "isContainsAudio": false,
-                "dirId": 7010102,
-                "juniorFlag": null,
-                "isContainsMovie": false,
-                "isContainsImage": false,
-                "previewContents": "명치아플땐?",
-                "entry": null,
-                "d4id": 7010102,
-                "tagList": null,
-                "docId": 303611001,
-                "formattedWriteTime": "오늘",
-                "metooWonderCnt": 0,
-                "d3id": 70101,
-                "openFlag": "N"
-            }
-
+        질문 목록 저장
+        :param question_list: 질문 목록
+        :param result_path: 저장 경로
+        :param update: 내용 업데이트 플래그
         :return:
         """
         import json
 
-        from os import listdir
-        from os.path import isdir, join
-
-        # 파일 로딩
-        count = 0
-        question_list = {}
-        for file in listdir(data_path):
-            filename = join(data_path, file)
-            if isdir(filename):
-                continue
-
-            logging.info(msg='filename: {}'.format(filename))
-
-            # 질문 로딩
-            with open(filename, 'r') as fp:
-                body = '\n'.join(fp.readlines())
-
-                q_list = json.loads(body)
-
-                # 카테고리별로 분리
-                for q in q_list:
-                    category_id = '{}'.format(q['dirId'])
-
-                    if category_id not in question_list:
-                        question_list[category_id] = []
-
-                    count += 1
-                    question_list[category_id].append(q)
-
-        logging.info(msg='question_list: category={:,}, total={:,}'.format(len(question_list), count))
-
         table_name = 'question_list'
-
-        for category_id in question_list:
-            db_filename = 'data/naver/kin/question_list/{}.sqlite3'.format(category_id)
+        for dir_id in question_list:
+            db_filename = '{}/{}.sqlite3'.format(result_path, dir_id)
             conn, cursor = self.open_sqlite(filename=db_filename, table_name=table_name)
 
             value_list = []
-            for question in question_list[category_id]:
+            for question in question_list[dir_id]:
                 # (d1_id, dir_id, doc_id, category, content)
                 content = json.dumps(question, ensure_ascii=False, sort_keys=True, indent=4)
                 values = (question['d1Id'], question['dirId'], question['docId'], content,)
@@ -1122,14 +1065,14 @@ class NaverKinCrawler(Utils):
 
                 if len(value_list) > 500:
                     self.batch_save_content(cursor=cursor, table_name=table_name,
-                                            value_list=value_list)
+                                            value_list=value_list, update=update)
                     conn.commit()
 
                     value_list = []
 
             if len(value_list) > 0:
                 self.batch_save_content(cursor=cursor, table_name=table_name,
-                                        value_list=value_list)
+                                        value_list=value_list, update=update)
                 conn.commit()
 
             conn.close()
@@ -1147,7 +1090,7 @@ def init_arguments():
 
     description = textwrap.dedent('''\
 # 질문 목록 크롤링
-python3 naver_kin_crawler.py -question
+python3 naver_kin_crawler.py -question_list -result_path data/naver/kin/question_list
 
 # 답변 목록 크롤링 
 python3 naver_kin_crawler.py -answer_list \\
@@ -1197,18 +1140,6 @@ python3 naver_kin_crawler.py -insert_user_list -data_path data/naver/kin/users/p
 python3 naver_kin_crawler.py -insert_user_list -data_path data/naver/kin/users/expert -category 경제
 python3 naver_kin_crawler.py -insert_user_list -data_path data/naver/kin/users/elite -category 년도별
 
-# 질문 목록을 DB에 저장
-python3 naver_kin_crawler.py -insert_question_list -data_path data/naver/kin/question_list/경제
-
-IFS=$'\\n'
-for d in $(ls -1 data/naver/kin/question_list.old) ; do
-    echo ${d}
-
-    python3 naver_kin_crawler.py -insert_question_list \\
-        -data_path data/naver/kin/question_list.old/${d}
-done
-
-
     ''')
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -1234,8 +1165,6 @@ done
                         help='질문 상세 페이지에서 정보 추출')
     parser.add_argument('-insert_user_list', action='store_true', default=False,
                         help='사용자 목록을 DB에 저장')
-    parser.add_argument('-insert_question_list', action='store_true', default=False,
-                        help='질문 목록을 DB에 저장')
 
     # 파라메터
     parser.add_argument('-filename', default='data/naver/test-detail.sqlite3', help='')
@@ -1252,7 +1181,7 @@ if __name__ == '__main__':
     crawler = NaverKinCrawler()
 
     if args.question_list:
-        crawler.batch_question_list()
+        crawler.batch_question_list(result_path=args.result_path)
 
     if args.answer_list:
         crawler.batch_answer_list(user_filename=args.filename, result_path=args.result_path)
@@ -1274,6 +1203,3 @@ if __name__ == '__main__':
 
     if args.insert_user_list:
         crawler.insert_user_list(data_path=args.data_path, category=args.category)
-
-    if args.insert_question_list:
-        crawler.insert_question_list(data_path=args.data_path)
