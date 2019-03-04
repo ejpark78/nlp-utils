687446fe9362ec9b2d87fb338357a946ac025628
ejpark <ejpark78@gmail.com>
2019-03-04 13:53:04 +0900

    filebeat 연동

--- module/logging_format.py
+++ module/logging_format.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import json
-from datetime import datetime
+# from datetime import datetime
 
 
 class LogMessage(object):
@@ -21,6 +21,6 @@ class LogMessage(object):
     def __str__(self):
         """"""
         self.message.update(self.kwargs)
-        self.message['log_date'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # self.message['log_date'] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')
 
         return json.dumps(self.message, ensure_ascii=False, sort_keys=True)
