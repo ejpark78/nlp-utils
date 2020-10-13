f08c918b4afa23ab9228c67bac3e1d974163e02d
ejpark <ejpark78@gmail.com>
2020-10-13 15:16:19 +0900

    ops 설정 파일 업데이트

--- ops/update-category/get_last_dt.sh
+++ ops/update-category/get_last_dt.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-kubectl get po | grep -v NAME | cut -f1 -d' ' | tee name
+kubectl get po | grep -v NAME | cut -f1 -d' ' | tee index
 
 kubectl get po | grep -v NAME | cut -f1 -d' ' \
-  | xargs -I{} echo "kubectl logs --tail=20 po/{} | grep ^{ | grep MESSAGE | head -n 1 | jq .date" \
+  | xargs -I{} echo "kubectl logs --tail=20 po/{} | grep ^{ | grep MESSAGE | head -n 1 | jq -r .date" \
   | sh - | tee dt
 
-paste name dt | tee last-date.txt
-rm name dt
+kubectl get po | grep -v NAME | cut -f1 -d' ' \
+  | xargs -I{} echo "kubectl logs --tail=20 po/{} | grep ^{ | grep MESSAGE | head -n 1 | jq -r .category" \
+  | sh - | tee category
+
+wc -l index dt category
+paste index category dt | tee last-date.txt
+
+rm index dt category
