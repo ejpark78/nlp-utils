92bfa1d502238cfc93d226cbbac1c5cb4d36733b
ejpark <ejpark78@gmail.com>
2020-11-17 09:57:14 +0900

    라이센스 변경: MIT -> Apache2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests*']),
     python_requires='>=3',
     url='http://galadriel02.korea.ncsoft.corp/searchtf/pypi/nlplab',
-    license='MIT',
+    license='Apache License Version 2.0',
     author='ejpark',
     author_email='ejpark@ncsoft.com',
     description='nlplab 데이터셋 배포 라이브러리'
