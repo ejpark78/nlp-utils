ed46d63aa64ba05d02e2f67c0d58d80d2e41b8e0
ejpark <ejpark78@gmail.com>
2020-12-03 15:47:28 +0900

    models 보강

--- src/nlplab/models.py
+++ src/nlplab/models.py
@@ -5,11 +5,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import bz2
-import json
-from os import getenv
-from os.path import isfile
 import tarfile
+from os import getenv
+from os.path import dirname
 
 import urllib3
 
@@ -46,39 +44,6 @@ class Models(object):
     def list(self):
         return [{name: self.info[name]['desc']} for name in self.info.keys()]
 
-    def load(self, name=None, tag=None, use_cache=True):
-        meta = self.get_meta(name=name)
-        if meta is None:
-            return None
-
-        self.use_cache = use_cache
-
-        if meta['location'] == 'minio':
-            return self.load_minio_data(meta=meta, tag=tag)
-
-        return None
-
-    def load_minio_data(self, meta, tag):
-        tag_list = [tag]
-        if meta is not None:
-            tag_list = meta['tags']
-
-        result = {}
-        for tag in tag_list:
-            filename = '{}/{}/{}.tar.bz2'.format(self.local_home, meta['local_path'], tag)
-
-            if isfile(filename) is False or self.use_cache is False:
-                self.pull_minio_file(tag=tag)
-
-            result[tag] = []
-            with tarfile.open(filename, 'r:bz2') as tar:
-                tar.extractall(path=self.local_home)
-
-        if len(result.keys()) == 1:
-            return result[list(result.keys())[0]]
-
-        return result
-
     def get_meta(self, name):
         if name is None:
             name = self.name
@@ -91,18 +56,23 @@ class Models(object):
 
         return self.info[name]
 
-    def pull_minio_file(self, tag, name=None):
+    def pull(self, tag, name=None):
         meta = self.get_meta(name=name)
         if meta is None:
             return
 
+        filename = '{}/{}/{}.tar.bz2'.format(self.local_home, meta['local_path'], tag)
         self.minio.pull(
-            local='{}/{}/{}.tar.bz2'.format(self.local_home, meta['local_path'], tag),
+            local=filename,
             remote='{}/{}/{}.tar.bz2'.format(self.remote_home, meta['remote_path'], tag),
         )
+
+        with tarfile.open(filename, 'r:bz2') as tar:
+            tar.extractall(path=dirname(filename))
+
         return
 
-    def push_minio_file(self, tag, name=None):
+    def push(self, tag, name=None):
         meta = self.get_meta(name=name)
         if meta is None:
             return
@@ -123,6 +93,6 @@ class Models(object):
             tag_list = meta['tags']
 
         for tag in tag_list:
-            self.push_minio_file(name=name, tag=tag)
+            self.push(name=name, tag=tag)
 
         return
