bc5b08a88bb8c498afb426be2bfd4e0db873a60f
ejpark <ejpark78@gmail.com>
2021-01-27 17:21:48 +0900

    국내 뉴스 추가

--- get_last_dt.sh
+++ get_last_dt.sh
@@ -5,12 +5,12 @@ kubectl get po | grep -v NAME | cut -f1 -d' ' | tee index
 kubectl get po | grep -v NAME | perl -ple 's/\s+/\t/g' | cut -f3 | tee status
 
 kubectl get po | grep -v NAME | cut -f1 -d' ' \
-  | xargs -I{} echo "kubectl logs --tail=100 po/{} | grep ^{ | sort -r | grep MESSAGE | grep category | head -n 1 | jq -r .date" \
-  | sh - | tee dt
+  | xargs -I{} echo "kubectl logs --tail=100 po/{} | grep ^{ | sort -r | grep MESSAGE | head -n 1 | jq -r .date" \
+  | bash - | tee dt
 
 kubectl get po | grep -v NAME | cut -f1 -d' ' \
-  | xargs -I{} echo "kubectl logs --tail=100 po/{} | grep ^{ | sort -r | grep MESSAGE | grep category | head -n 1 | jq -r .category" \
-  | sh - | tee category
+  | xargs -I{} echo "kubectl logs --tail=100 po/{} | grep ^{ | sort -r | grep MESSAGE | head -n 1 | jq -r .category" \
+  | bash - | tee category
 
 wc -l index status dt category
 paste index status category dt | tee last-date.csv
