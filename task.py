75548681cf021aaefef73af31979567f0a6bb448
ejpark <ejpark78@gmail.com>
2021-02-08 19:13:08 +0900

    update

--- task.py
+++ task.py
@@ -1,7 +1,12 @@
+from datetime import timedelta
+
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
+
+## ref: https://bomwo.cc/posts/kubernetespodoperator/
+## https://stackoverflow.com/questions/56296775/airflow-modulenotfounderror-no-module-named-kubernetes
 
 dag = DAG(
     dag_id='task',
@@ -9,11 +14,13 @@ dag = DAG(
     default_args={
         'owner': 'Airflow',
         'retries': 3,
+        'retry_delay': timedelta(minutes=5),
         'start_date': days_ago(1),
         'depends_on_past': False,
         'email': ['ejpark@ncsoft.com'],
         'email_on_retry': False,
-        'email_on_failure': False
+        'email_on_failure': False,
+        'execution_timeout': timedelta(hours=1)
     },
     schedule_interval='0,30 * * * *',
     max_active_runs=1
@@ -22,15 +29,16 @@ dag = DAG(
 start = DummyOperator(task_id='start', dag=dag)
 
 run = KubernetesPodOperator(
-    namespace='airflow',
-    image="registry.nlp-utils/crawler:dev",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    name="test",
+    name="job",
     task_id="task",
+    namespace='airflow',
+    image='registry.nlp-utils/crawler:dev',
     is_delete_operator_pod=True,
-    hostnetwork=False,
+    image_pull_secrets='registry',
+    get_logs=True,
     dag=dag,
 )
 
-start >> run
+end = DummyOperator(task_id='end', dag=dag)
+
+start >> run >> end
