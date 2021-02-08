78d1c6c965ded0fcea3ae8ff0ec55e9662825787
ejpark <ejpark78@gmail.com>
2021-02-08 18:50:57 +0900

    update

--- task.py
+++ task.py
@@ -1,7 +1,5 @@
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.kubernetes.secret import Secret
-
-secret_env = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
+from airflow.operators.dummy_operator import DummyOperator
 
 configmaps = []
 
@@ -10,11 +8,11 @@ k = KubernetesPodOperator(
     image="registry.nlp-utils/crawler:dev",
     cmds=["bash", "-cx"],
     arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    secrets=[secret_env],
     name="test",
     task_id="task",
     is_delete_operator_pod=True,
     hostnetwork=False,
     configmaps=configmaps
 )
+
+k
