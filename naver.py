caf0197636ca45e9d0b1b0928dab39bc7319a66d
ejpark <ejpark78@gmail.com>
2021-02-09 15:12:25 +0900

    add task update

--- naver.py
+++ naver.py
@@ -7,7 +7,7 @@ from airflow.utils.dates import days_ago
 
 dag = DAG(
     dag_id='naver',
-    description='kubernetes pod operator',
+    description='naver crawler',
     default_args={
         'owner': 'Airflow',
         'retries': 3,
@@ -22,15 +22,6 @@ dag = DAG(
     schedule_interval='0,30 * * * *',
     max_active_runs=1
 )
-
-start = DummyOperator(task_id='start', dag=dag)
-end = DummyOperator(task_id='end', dag=dag)
-
-category_list = {}
-for task_id in 'economy,international,it,living,opinion,politics,society,sports,tv,weather'.split(','):
-    category_list[task_id] = DummyOperator(task_id=task_id, dag=dag)
-
-    start >> category_list[task_id]
 
 env_vars = {
     'ELASTIC_SEARCH_HOST': 'https://corpus.ncsoft.com:9200',
@@ -55,83 +46,328 @@ args = [
     '10',
 ]
 
+start = DummyOperator(task_id='start', dag=dag)
+end = DummyOperator(task_id='end', dag=dag)
+
+category_list = {}
+for task_id in 'economy,international,it,living,opinion,politics,society,sports,tv,weather'.split(','):
+    category_list[task_id] = DummyOperator(task_id=task_id, dag=dag)
+
+    start >> category_list[task_id]
+
 sub_category = [
-    {'category': 'economy', 'task_id': 'stock', 'name': "경제/증권", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'finance', 'name': "경제/금융", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'estate', 'name': "경제/부동산", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'industry', 'name': "경제/산업/재계", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'global', 'name': "경제/글로벌 경제", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'general', 'name': "경제/경제 일반", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'economy-living', 'name': "경제/생활경제", 'config': '/config/naver/economy.yaml'},
-    {'category': 'economy', 'task_id': 'venture', 'name': "경제/중기/벤처", 'config': '/config/naver/economy.yaml'},
+    {
+        'category': 'economy',
+        'task_id': 'stock',
+        'name': '경제/증권',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'finance',
+        'name': '경제/금융',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'estate',
+        'name': '경제/부동산',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'industry',
+        'name': '경제/산업/재계',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'global',
+        'name': '경제/글로벌 경제',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'general',
+        'name': '경제/경제 일반',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'economy-living',
+        'name': '경제/생활경제',
+    },
+    {
+        'category': 'economy',
+        'task_id': 'venture',
+        'name': '경제/중기/벤처',
+    },
 
-    {'category': 'international', 'task_id': 'asia', 'name': "세계/아시아호주", 'config': '/config/naver/international.yaml'},
-    {'category': 'international', 'task_id': 'western', 'name': "세계/미국중남미", 'config': '/config/naver/international.yaml'},
-    {'category': 'international', 'task_id': 'europe', 'name': "세계/유럽", 'config': '/config/naver/international.yaml'},
-    {'category': 'international', 'task_id': 'mideast', 'name': "세계/중동아프리카", 'config': '/config/naver/international.yaml'},
-    {'category': 'international', 'task_id': 'international-etc', 'name': "세계/일반", 'config': '/config/naver/international.yaml'},
+    {
+        'category': 'international',
+        'task_id': 'asia',
+        'name': '세계/아시아호주',
+    },
+    {
+        'category': 'international',
+        'task_id': 'western',
+        'name': '세계/미국중남미',
+    },
+    {
+        'category': 'international',
+        'task_id': 'europe',
+        'name': '세계/유럽',
+    },
+    {
+        'category': 'international',
+        'task_id': 'mideast',
+        'name': '세계/중동아프리카',
+    },
+    {
+        'category': 'international',
+        'task_id': 'international-etc',
+        'name': '세계/일반',
+    },
 
-    {'category': 'it', 'task_id': 'mobile', 'name': "IT/모바일", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'sns', 'name': "IT/인터넷/SNS", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'new-media', 'name': "IT/통신/뉴미디어", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'it-etc', 'name': "IT/IT 일반", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'security', 'name': "IT/보안/해킹", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'computer', 'name': "IT/컴퓨터", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'review', 'name': "IT/게임/리뷰", 'config': '/config/naver/it.yaml'},
-    {'category': 'it', 'task_id': 'science', 'name': "IT/과학 일반", 'config': '/config/naver/it.yaml'},
+    {
+        'category': 'it',
+        'task_id': 'mobile',
+        'name': 'IT/모바일',
+    },
+    {
+        'category': 'it',
+        'task_id': 'sns',
+        'name': 'IT/인터넷/SNS',
+    },
+    {
+        'category': 'it',
+        'task_id': 'new-media',
+        'name': 'IT/통신/뉴미디어',
+    },
+    {
+        'category': 'it',
+        'task_id': 'it-etc',
+        'name': 'IT/IT 일반',
+    },
+    {
+        'category': 'it',
+        'task_id': 'security',
+        'name': 'IT/보안/해킹',
+    },
+    {
+        'category': 'it',
+        'task_id': 'computer',
+        'name': 'IT/컴퓨터',
+    },
+    {
+        'category': 'it',
+        'task_id': 'review',
+        'name': 'IT/게임/리뷰',
+    },
+    {
+        'category': 'it',
+        'task_id': 'science',
+        'name': 'IT/과학 일반',
+    },
 
-    {'category': 'living', 'task_id': 'health', 'name': "생활/건강정보", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'car', 'name': "생활/자동차시승기", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'traffic', 'name': "생활/도로교통", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'tour', 'name': "생활/여행/레저", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'food', 'name': "생활/음식맛집", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'beauty', 'name': "생활/패션뷰티", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'art', 'name': "생활/공연전시", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'book', 'name': "생활/책", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'religion', 'name': "생활/종교", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'living-forecast', 'name': "생활/날씨", 'config': '/config/naver/living.yaml'},
-    {'category': 'living', 'task_id': 'living-etc', 'name': "생활/일반", 'config': '/config/naver/living.yaml'},
+    {
+        'category': 'living',
+        'task_id': 'health',
+        'name': '생활/건강정보',
+    },
+    {
+        'category': 'living',
+        'task_id': 'car',
+        'name': '생활/자동차시승기',
+    },
+    {
+        'category': 'living',
+        'task_id': 'traffic',
+        'name': '생활/도로교통',
+    },
+    {
+        'category': 'living',
+        'task_id': 'tour',
+        'name': '생활/여행/레저',
+    },
+    {
+        'category': 'living',
+        'task_id': 'food',
+        'name': '생활/음식맛집',
+    },
+    {
+        'category': 'living',
+        'task_id': 'beauty',
+        'name': '생활/패션뷰티',
+    },
+    {
+        'category': 'living',
+        'task_id': 'art',
+        'name': '생활/공연전시',
+    },
+    {
+        'category': 'living',
+        'task_id': 'book',
+        'name': '생활/책',
+    },
+    {
+        'category': 'living',
+        'task_id': 'religion',
+        'name': '생활/종교',
+    },
+    {
+        'category': 'living',
+        'task_id': 'living-forecast',
+        'name': '생활/날씨',
+    },
+    {
+        'category': 'living',
+        'task_id': 'living-etc',
+        'name': '생활/일반',
+    },
 
-    {'category': 'politics', 'task_id': 'blue-house', 'name': "정치/청와대", 'config': '/config/naver/politics.yaml'},
-    {'category': 'politics', 'task_id': 'national-assembly', 'name': "정치/국회정당", 'config': '/config/naver/politics.yaml'},
-    {'category': 'politics', 'task_id': 'north-korea', 'name': "정치/북한", 'config': '/config/naver/politics.yaml'},
-    {'category': 'politics', 'task_id': 'administration', 'name': "정치/행정", 'config': '/config/naver/politics.yaml'},
-    {'category': 'politics', 'task_id': 'national-defense', 'name': "정치/국방외교", 'config': '/config/naver/politics.yaml'},
-    {'category': 'politics', 'task_id': 'politics-etc', 'name': "정치/정치일반", 'config': '/config/naver/politics.yaml'},
+    {
+        'category': 'politics',
+        'task_id': 'blue-house',
+        'name': '정치/청와대',
+    },
+    {
+        'category': 'politics',
+        'task_id': 'national-assembly',
+        'name': '정치/국회정당',
+    },
+    {
+        'category': 'politics',
+        'task_id': 'north-korea',
+        'name': '정치/북한',
+    },
+    {
+        'category': 'politics',
+        'task_id': 'administration',
+        'name': '정치/행정',
+    },
+    {
+        'category': 'politics',
+        'task_id': 'national-defense',
+        'name': '정치/국방외교',
+    },
+    {
+        'category': 'politics',
+        'task_id': 'politics-etc',
+        'name': '정치/정치일반',
+    },
 
-    {'category': 'society', 'task_id': 'incident', 'name': "사회/사건사고", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'education', 'name': "사회/교육", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'labor', 'name': "사회/노동", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'media', 'name': "사회/언론", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'environment', 'name': "사회/환경", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'human-right', 'name': "사회/인권복지", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'medical', 'name': "사회/식품의료", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'local', 'name': "사회/지역", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'person', 'name': "사회/인물", 'config': '/config/naver/society.yaml'},
-    {'category': 'society', 'task_id': 'society-etc', 'name': "사회/사회일반", 'config': '/config/naver/society.yaml'},
+    {
+        'category': 'society',
+        'task_id': 'incident',
+        'name': '사회/사건사고',
+    },
+    {
+        'category': 'society',
+        'task_id': 'education',
+        'name': '사회/교육',
+    },
+    {
+        'category': 'society',
+        'task_id': 'labor',
+        'name': '사회/노동',
+    },
+    {
+        'category': 'society',
+        'task_id': 'media',
+        'name': '사회/언론',
+    },
+    {
+        'category': 'society',
+        'task_id': 'environment',
+        'name': '사회/환경',
+    },
+    {
+        'category': 'society',
+        'task_id': 'human-right',
+        'name': '사회/인권복지',
+    },
+    {
+        'category': 'society',
+        'task_id': 'medical',
+        'name': '사회/식품의료',
+    },
+    {
+        'category': 'society',
+        'task_id': 'local',
+        'name': '사회/지역',
+    },
+    {
+        'category': 'society',
+        'task_id': 'person',
+        'name': '사회/인물',
+    },
+    {
+        'category': 'society',
+        'task_id': 'society-etc',
+        'name': '사회/사회일반',
+    },
 
-    {'category': 'weather', 'task_id': 'forecast', 'name': "날씨", 'config': '/config/naver/weather.yaml'},
-    {'category': 'opinion', 'task_id': 'opi', 'name': "날씨", 'config': '/config/naver/opinion.yaml'},
+    {
+        'category': 'weather',
+        'task_id': 'forecast',
+        'name': '날씨',
+    },
+    {
+        'category': 'opinion',
+        'task_id': 'opi',
+        'name': '날씨',
+    },
 
-    {'category': 'sports', 'task_id': 'baseball', 'name': "스포츠/야구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'wbaseball', 'name': "스포츠/해외야구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'game', 'name': "스포츠/게임", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'football', 'name': "스포츠/축구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'wfootball', 'name': "스포츠/해외축구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'basketball', 'name': "스포츠/농구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'volleyball', 'name': "스포츠/배구", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'golf', 'name': "스포츠/골프", 'config': '/config/naver/sports.yaml'},
-    {'category': 'sports', 'task_id': 'sports-etc', 'name': "스포츠/스포츠-일반", 'config': '/config/naver/sports.yaml'},
+    {
+        'category': 'sports',
+        'task_id': 'baseball',
+        'name': '스포츠/야구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'wbaseball',
+        'name': '스포츠/해외야구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'game',
+        'name': '스포츠/게임',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'football',
+        'name': '스포츠/축구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'wfootball',
+        'name': '스포츠/해외축구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'basketball',
+        'name': '스포츠/농구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'volleyball',
+        'name': '스포츠/배구',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'golf',
+        'name': '스포츠/골프',
+    },
+    {
+        'category': 'sports',
+        'task_id': 'sports-etc',
+        'name': '스포츠/스포츠-일반',
+    },
 ]
 
 for item in sub_category:
     task = KubernetesPodOperator(
         dag=dag,
-        name='app',
+        name='task',
         task_id=item['task_id'],
         arguments=args + [
             '--config',
-            item['config'],
+            '/config/naver/{}.yaml'.format(item['category']),
             '--sub-category',
             item['name'],
         ],
