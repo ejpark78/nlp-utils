14e93a4565ca014cbf1f73d219c240742c11f08d
ejpark <ejpark78@gmail.com>
2021-02-10 17:21:16 +0900

    add task update

--- test.py
+++ test.py
@@ -6,10 +6,8 @@ from __future__ import division
 from __future__ import print_function
 
 import logging
-import os
 
 from airflow import DAG
-from airflow.example_dags.libs.helper import print_stuff
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
@@ -33,7 +31,6 @@ try:
         # You can use annotations on your kubernetes pods!
         start_task = PythonOperator(
             task_id="start_task",
-            python_callable=print_stuff,
             executor_config={
                 "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={"test": "annotation"}))
             },
@@ -42,7 +39,6 @@ try:
         # [START task_with_sidecar]
         sidecar_task = PythonOperator(
             task_id="task_with_sidecar",
-            python_callable=print_stuff,
             executor_config={
                 "pod_override": k8s.V1Pod(
                     spec=k8s.V1PodSpec(
@@ -60,26 +56,6 @@ try:
         )
         # [END task_with_sidecar]
 
-        # Test that we can add labels to pods
-        third_task = PythonOperator(
-            task_id="non_root_task",
-            python_callable=print_stuff,
-            executor_config={
-                "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(labels={"release": "stable"}))
-            },
-        )
-
-        other_ns_task = PythonOperator(
-            task_id="other_namespace_task",
-            python_callable=print_stuff,
-            executor_config={
-                "KubernetesExecutor": {
-                    "namespace": "test-namespace", "labels": {"release": "stable"}
-                }
-            },
-        )
-
-        start_task >> other_ns_task
         start_task >> sidecar_task
 except ImportError as e:
     log.warning("Could not import DAGs in example_kubernetes_executor_config.py: %s", str(e))
