eb697da1eafbe73fed71be58c7d5f6b90eca9d74
ejpark <ejpark78@gmail.com>
2021-02-10 10:13:11 +0900

    add task update

--- utils.py
+++ utils.py
@@ -6,8 +6,12 @@ from __future__ import division
 from __future__ import print_function
 
 from os import getenv
-
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
 import yaml
+from datetime import timedelta
 
 
 def open_config(filename: str) -> dict:
@@ -19,3 +23,38 @@ def open_config(filename: str) -> dict:
     with open(filename, 'r') as fp:
         data = yaml.load(stream=fp, Loader=yaml.FullLoader)
         return dict(data)
+
+
+def build_dag(config: dict) -> None:
+    default_args = {
+        **config['default_args'],
+        'retry_delay': timedelta(minutes=10),
+        'start_date': days_ago(n=0, hour=1),
+        'execution_timeout': timedelta(hours=1)
+    }
+
+    with DAG(**config['dag'], default_args=default_args) as dag:
+        start = DummyOperator(task_id='start', dag=dag)
+
+        category_operator = {}
+        for item in config['tasks']:
+            name = item['category']
+
+            if name not in category_operator:
+                category_operator[name] = DummyOperator(task_id=name, dag=dag)
+                start >> category_operator[name]
+
+            category_operator[name] >> KubernetesPodOperator(
+                dag=dag,
+                name='task',
+                task_id=item['task_id'],
+                arguments=config['operator']['args'] + [
+                    '--config',
+                    item['config'],
+                    '--sub-category',
+                    item['name'],
+                ],
+                **config['operator']['params']
+            )
+
+    return
