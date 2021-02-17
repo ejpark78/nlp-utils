15f071f615321d85daf42b433a438062396d8597
ejpark <ejpark78@gmail.com>
2021-02-17 14:18:21 +0900

    add korea news

---
+++ news/hankook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+from datetime import timedelta
+
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+from utils import open_config
+
+log = logging.getLogger(__name__)
+
+config = open_config(filename='config/news/hankook.yaml')
+
+default_args = {
+    **config['default_args'],
+    'start_date': days_ago(n=1),
+    'retry_delay': timedelta(minutes=10),
+    'execution_timeout': timedelta(hours=1)
+}
+
+with DAG(**config['dag'], default_args=default_args) as dag:
+    start = DummyOperator(task_id='start', dag=dag)
+
+    prev = start
+
+    category_operator = {}
+    for item in config['tasks']:
+        name = item['category']
+
+        if name not in category_operator:
+            category_operator[name] = DummyOperator(task_id=name, dag=dag)
+            start >> category_operator[name]
+            prev = category_operator[name]
+
+        task = KubernetesPodOperator(
+            dag=dag,
+            name='task',
+            task_id=item['task_id'],
+            arguments=config['operator']['args'] + [
+                '--config',
+                item['config'],
+                '--sub-category',
+                item['name'],
+            ],
+            **config['operator']['params']
+        )
+
+        prev >> task
+        prev = task
--- test.py
+++ test.py
@@ -1,65 +1,65 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import logging
-
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
-from airflow.contrib.example_dags.libs.helper import print_stuff
-
-default_args = {
-    'owner': 'airflow',
-}
-
-log = logging.getLogger(__name__)
-
-try:
-    from kubernetes.client import models as k8s
-
-    with DAG(
-        dag_id='example_kubernetes_executor_config',
-        default_args=default_args,
-        schedule_interval=None,
-        start_date=days_ago(2),
-        tags=['example3'],
-    ) as dag:
-
-        # You can use annotations on your kubernetes pods!
-        start_task = PythonOperator(
-            task_id="start_task",
-            python_callable=print_stuff,
-            executor_config={
-                "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={"test": "annotation"}))
-            },
-        )
-
-        # [START task_with_sidecar]
-        sidecar_task = PythonOperator(
-            task_id="task_with_sidecar",
-            python_callable=print_stuff,
-            executor_config={
-                "pod_override": k8s.V1Pod(
-                    spec=k8s.V1PodSpec(
-                        containers=[
-                            k8s.V1Container(
-                                name="app",
-                                image="registry.nlp-utils/crawler:live",
-                                args=["ls /config/news/"],
-                                command=["bash", "-cx"]
-                            ),
-                        ]
-                    )
-                ),
-            },
-        )
-        # [END task_with_sidecar]
-
-        start_task >> sidecar_task
-except ImportError as e:
-    log.warning("Could not import DAGs in example_kubernetes_executor_config.py: %s", str(e))
-    log.warning("Install kubernetes dependencies with: pip install apache-airflow['cncf.kubernetes']")
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+#
+# import logging
+#
+# from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.utils.dates import days_ago
+# from airflow.contrib.example_dags.libs.helper import print_stuff
+#
+# default_args = {
+#     'owner': 'airflow',
+# }
+#
+# log = logging.getLogger(__name__)
+#
+# try:
+#     from kubernetes.client import models as k8s
+#
+#     with DAG(
+#         dag_id='example_kubernetes_executor_config',
+#         default_args=default_args,
+#         schedule_interval=None,
+#         start_date=days_ago(2),
+#         tags=['example3'],
+#     ) as dag:
+#
+#         # You can use annotations on your kubernetes pods!
+#         start_task = PythonOperator(
+#             task_id="start_task",
+#             python_callable=print_stuff,
+#             executor_config={
+#                 "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={"test": "annotation"}))
+#             },
+#         )
+#
+#         # [START task_with_sidecar]
+#         sidecar_task = PythonOperator(
+#             task_id="task_with_sidecar",
+#             python_callable=print_stuff,
+#             executor_config={
+#                 "pod_override": k8s.V1Pod(
+#                     spec=k8s.V1PodSpec(
+#                         containers=[
+#                             k8s.V1Container(
+#                                 name="app",
+#                                 image="registry.nlp-utils/crawler:live",
+#                                 args=["ls /config/news/"],
+#                                 command=["bash", "-cx"]
+#                             ),
+#                         ]
+#                     )
+#                 ),
+#             },
+#         )
+#         # [END task_with_sidecar]
+#
+#         start_task >> sidecar_task
+# except ImportError as e:
+#     log.warning("Could not import DAGs in example_kubernetes_executor_config.py: %s", str(e))
+#     log.warning("Install kubernetes dependencies with: pip install apache-airflow['cncf.kubernetes']")
