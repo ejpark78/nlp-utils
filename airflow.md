bca1c10155d1ea167d5ac09e06dbe2254ff3733f
ejpark <ejpark78@gmail.com>
2021-02-18 10:49:18 +0900

    airflow doc update

---
+++ airflow.md
@@ -0,0 +1,52 @@
+# airflow
+
+## local
+
+* https://airflow.apache.org/docs/apache-airflow/stable/start/local.html
+
+```bash
+source venv/bin/activate
+
+pip3 install -U -r requirements.dev.txt
+
+export AIRFLOW_HOME=$(pwd)/airflow
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
+export AIRFLOW__KUBERNETES__GIT_DAGS_FOLDER_MOUNT_POINT=$(pwd) 
+export AIRFLOW__KUBERNETES__GIT_DAGS_VOLUME_SUBPATH=dags 
+export AIRFLOW_DAGS_FOLDER=$(pwd)/dags
+
+rm airflow/airflow.db
+airflow db init
+
+echo '\n\n' | airflow users create \
+    --username admin \
+    --lastname 박 \
+    --firstname 은진 \
+    --role Admin \
+    --email ejpark@ncsoft.com
+
+airflow webserver --port 8080
+
+airflow scheduler
+
+airflow dags list
+airflow dags report  
+  
+```
+
+## docker
+
+* https://airflow.apache.org/docs/apache-airflow/stable/start/docker.html
+
+```bash
+curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.0.1/docker-compose.yaml'
+```
+
+* https://github.com/bitnami/bitnami-docker-airflow
+
+```bash
+curl -sSL https://raw.githubusercontent.com/bitnami/bitnami-docker-airflow/master/1/debian-10/docker-compose.yml > docker-compose.yml
+
+docker-compose up -d
+```
+
--- crawler/utils/yaml2dag.py
+++ crawler/utils/yaml2dag.py
@@ -16,64 +16,88 @@ class Yaml2Dag(object):
     def __init__(self):
         super().__init__()
 
-        self.index = {
-            '일반': 'etc',
-            'SNS': 'sns',
-            '건강정보': 'health',
-            '게임': 'game',
-            '골프': 'golf',
-            '공연전시': 'art',
-            '영화': 'movie',
-            '과학': 'science',
-            '교육': 'education',
-            '국방외교': 'national-defense',
-            '국회정당': 'national-assembly',
-            '글로벌': 'global',
-            '금융': 'finance',
-            '날씨': 'forecast',
-            '노동': 'labor',
-            '농구': 'basketball',
-            '뉴미디어': 'new-media',
-            '도로교통': 'traffic',
-            '레저': 'tour',
-            '리뷰': 'review',
-            '모바일': 'mobile',
-            '미국': 'usa',
-            '중남미': 'western',
-            '배구': 'volleyball',
-            '벤처': 'venture',
-            '부동산': 'estate',
-            '북한': 'north-korea',
-            '사건사고': 'incident',
-            '생활': 'living',
-            '식품의료': 'medical',
-            '아시아호주': 'asia',
-            '야구': 'baseball',
-            '언론': 'media',
-            '유럽': 'europe',
-            '음식맛집': 'food',
-            '인권복지': 'human-right',
-            '인물': 'person',
-            '자동차시승기': 'car',
-            '재계': 'industry',
-            '종교': 'religion',
-            '중동아프리카': 'mideast',
-            '증권': 'stock',
-            '지역': 'local',
-            '책': 'book',
-            '청와대': 'blue-house',
-            '축구': 'football',
-            '컴퓨터': 'computer',
-            '패션뷰티': 'beauty',
-            '해외야구': 'wbaseball',
-            '해외축구': 'wfootball',
-            '해킹': 'security',
-            '행정': 'administration',
-            '환경': 'environment',
-            '경제': 'economy',
-            '기업': 'company',
-            '창업': 'startup',
-        }
+        self.alias = [
+            {'text': '해외야구', 'to': 'wbaseball'},
+            {'text': '해외축구', 'to': 'wfootball'},
+            {'text': '국방외교', 'to': 'national-defense'},
+            {'text': '국회정당', 'to': 'national-assembly'},
+            {'text': '자동차시승기', 'to': 'car'},
+            {'text': '중동아프리카', 'to': 'mideast'},
+            {'text': '패션뷰티', 'to': 'beauty'},
+            {'text': '여행', 'to': 'travel'},
+            {'text': '공연전시', 'to': 'art'},
+            {'text': 'SNS', 'to': 'sns'},
+            {'text': '건강정보', 'to': 'health'},
+            {'text': '게임', 'to': 'game'},
+            {'text': '골프', 'to': 'golf'},
+            {'text': '영화', 'to': 'movie'},
+            {'text': '과학', 'to': 'science'},
+            {'text': '교육', 'to': 'education'},
+            {'text': '글로벌', 'to': 'global'},
+            {'text': '금융', 'to': 'finance'},
+            {'text': '날씨', 'to': 'forecast'},
+            {'text': '노동', 'to': 'labor'},
+            {'text': '농구', 'to': 'basketball'},
+            {'text': '뉴미디어', 'to': 'new-media'},
+            {'text': '도로교통', 'to': 'traffic'},
+            {'text': '레저', 'to': 'tour'},
+            {'text': '리뷰', 'to': 'review'},
+            {'text': '모바일', 'to': 'mobile'},
+            {'text': '미국', 'to': 'usa'},
+            {'text': '중남미', 'to': 'western'},
+            {'text': '배구', 'to': 'volleyball'},
+            {'text': '벤처', 'to': 'venture'},
+            {'text': '부동산', 'to': 'estate'},
+            {'text': '북한', 'to': 'north-korea'},
+            {'text': '사건사고', 'to': 'incident'},
+            {'text': '생활', 'to': 'living'},
+            {'text': '식품의료', 'to': 'medical'},
+            {'text': '아시아호주', 'to': 'asia'},
+            {'text': '야구', 'to': 'baseball'},
+            {'text': '언론', 'to': 'media'},
+            {'text': '유럽', 'to': 'europe'},
+            {'text': '음식맛집', 'to': 'food'},
+            {'text': '인권복지', 'to': 'human-right'},
+            {'text': '인물', 'to': 'person'},
+            {'text': '재계', 'to': 'industry'},
+            {'text': '종교', 'to': 'religion'},
+            {'text': '증권', 'to': 'stock'},
+            {'text': '지역', 'to': 'local'},
+            {'text': '책', 'to': 'book'},
+            {'text': '청와대', 'to': 'blue-house'},
+            {'text': '축구', 'to': 'football'},
+            {'text': '컴퓨터', 'to': 'computer'},
+            {'text': '해킹', 'to': 'security'},
+            {'text': '행정', 'to': 'administration'},
+            {'text': '환경', 'to': 'environment'},
+            {'text': '경제', 'to': 'economy'},
+            {'text': '기업', 'to': 'company'},
+            {'text': '창업', 'to': 'startup'},
+            {'text': '사람들', 'to': 'people'},
+            {'text': '종합', 'to': 'total'},
+            {'text': '문화', 'to': 'culture'},
+            {'text': '일반', 'to': 'etc'},
+            {'text': '스포츠', 'to': 'sports'},
+            {'text': '사회', 'to': 'society'},
+            {'text': '마켓', 'to': 'market'},
+            {'text': '산업', 'to': 'industry'},
+            {'text': '하이테크', 'to': 'hitech'},
+            {'text': '차이나포커스', 'to': 'china-focus'},
+            {'text': '철강', 'to': 'steel'},
+            {'text': '유통', 'to': 'distribution'},
+            {'text': '바이오', 'to': 'bio'},
+            {'text': '외교', 'to': 'diplomacy'},
+            {'text': '국회', 'to': 'national-assembly'},
+            {'text': '정당', 'to': 'party'},
+            {'text': '정치', 'to': 'politics'},
+            {'text': '연예', 'to': 'entertainment'},
+            {'text': '상품', 'to': 'product'},
+            {'text': '금융', 'to': 'finance'},
+            {'text': '보험', 'to': 'insurance'},
+            {'text': '이슈', 'to': 'issue'},
+            {'text': '오피니언', 'to': 'opinion'},
+
+        ]
 
     @staticmethod
     def open_config(filename: str) -> dict:
@@ -81,9 +105,20 @@ class Yaml2Dag(object):
             data = yaml.load(stream=fp, Loader=yaml.FullLoader)
             return dict(data)
 
+    def get_task_id(self, name: str, i: int, category: str) -> str:
+        task_id = '{}-{}'.format(name, i)
+
+        for x in self.alias:
+            if x['text'] not in category:
+                continue
+
+            return '{}-{}'.format(name, x['to'])
+
+        return task_id
+
     def batch(self) -> None:
         stop_list = '/_,-reply,blog,cafe,terms'.split(',')
-        for filename in glob('config/news/*.yaml'):
+        for filename in glob('config/world_news/upi.yaml'):
             skip = False
             for x in stop_list:
                 if x in filename:
@@ -106,24 +141,15 @@ class Yaml2Dag(object):
                 if 'category' not in item:
                     continue
 
-                task_id = category
-
-                # task_id = '{}-{}'.format(category, i)
-
-                # for x in self.index.keys():
-                #     if x not in item['category']:
-                #         continue
-                #
-                #     task_id = '{}-{}'.format(category, self.index[x])
-                #     break
+                task_id = self.get_task_id(i=item['category'], name=category, category=item['category'])
 
                 conf.append({
-                    # 'name': item['category'],
+                    # 'category': task_id.split('-', maxsplit=1)[-1],
+                    'category': item['category'],
+                    'name': item['category'],
                     'task_id': task_id,
-                    # 'category': category,
                     'config': '/' + filename
                 })
-                break
 
             yaml.dump(
                 data=conf,
