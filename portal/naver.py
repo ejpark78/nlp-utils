ec950cdbc0b77a99427b9e11abf1fc3e478970d2
ejpark <ejpark78@gmail.com>
2021-02-19 09:38:37 +0900

    change tag

--- portal/naver.py
+++ portal/naver.py
@@ -5,51 +5,49 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import logging
 from datetime import timedelta
 
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
-from utils import open_config
+from utils import open_config, build_portal_dags
 
-log = logging.getLogger(__name__)
+build_portal_dags(filename='config/portal/naver.yaml')
 
-config = open_config(filename='config/portal/naver.yaml')
 
-default_args = {
-    **config['default_args'],
-    'start_date': days_ago(n=1),
-    'retry_delay': timedelta(minutes=10),
-    'execution_timeout': timedelta(hours=1)
-}
-
-with DAG(**config['dag'], default_args=default_args) as dag:
-    start = DummyOperator(task_id='start', dag=dag)
-
-    task_group = {}
-    category_group = {}
-    for item in config['tasks']:
-        name = item['category']
-        if name not in task_group:
-            task_group[name] = []
-
-            category_group[name] = DummyOperator(task_id=name, dag=dag)
-
-        task_group[name].append(KubernetesPodOperator(
-            dag=dag,
-            name='task',
-            task_id=item['task_id'],
-            arguments=config['operator']['args'] + [
-                '--config',
-                item['config'],
-                '--sub-category',
-                item['name'],
-            ],
-            **config['operator']['params']
-        ))
-
-    start >> category_group.values()
-    for name in task_group:
-        category_group[name] >> task_group[name]
+# default_args = {
+#     **config['default_args'],
+#     'start_date': days_ago(n=1),
+#     'retry_delay': timedelta(minutes=10),
+#     'execution_timeout': timedelta(hours=1)
+# }
+#
+# with DAG(**config['dag'], default_args=default_args) as dag:
+#     start = DummyOperator(task_id='start', dag=dag)
+#
+#     task_group = {}
+#     category_group = {}
+#     for item in config['tasks']:
+#         name = item['category']
+#         if name not in task_group:
+#             task_group[name] = []
+#
+#             category_group[name] = DummyOperator(task_id=name, dag=dag)
+#
+#         task_group[name].append(KubernetesPodOperator(
+#             dag=dag,
+#             name='task',
+#             task_id=item['task_id'],
+#             arguments=config['operator']['args'] + [
+#                 '--config',
+#                 item['config'],
+#                 '--sub-category',
+#                 item['name'],
+#             ],
+#             **config['operator']['params']
+#         ))
+#
+#     start >> category_group.values()
+#     for name in task_group:
+#         category_group[name] >> task_group[name]
--- utils.py
+++ utils.py
@@ -23,3 +23,45 @@ def open_config(filename: str) -> dict:
     with open(filename, 'r') as fp:
         data = yaml.load(stream=fp, Loader=yaml.FullLoader)
         return dict(data)
+
+
+def build_portal_dags(filename: str) -> None:
+    config = open_config(filename=filename)
+
+    default_args = {
+        **config['default_args'],
+        'start_date': days_ago(n=1),
+        'retry_delay': timedelta(minutes=10),
+        'execution_timeout': timedelta(hours=1)
+    }
+
+    with DAG(**config['dag'], default_args=default_args) as dag:
+        start = DummyOperator(task_id='start', dag=dag)
+
+        task_group = {}
+        category_group = {}
+        for item in config['tasks']:
+            name = item['category']
+            if name not in task_group:
+                task_group[name] = []
+
+                category_group[name] = DummyOperator(task_id=name, dag=dag)
+
+            task_group[name].append(KubernetesPodOperator(
+                dag=dag,
+                name='task',
+                task_id=item['task_id'],
+                arguments=config['operator']['args'] + [
+                    '--config',
+                    item['config'],
+                    '--sub-category',
+                    item['name'],
+                ],
+                **config['operator']['params']
+            ))
+
+        start >> category_group.values()
+        for name in task_group:
+            category_group[name] >> task_group[name]
+
+    return
