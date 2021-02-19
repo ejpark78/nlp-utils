1ed4c248b399aba279b01bf0dfad3d9c3ed8ae18
ejpark <ejpark78@gmail.com>
2021-02-19 10:29:27 +0900

    change tag

--- portal/naver.py
+++ portal/naver.py
@@ -5,55 +5,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from os import getenv
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.models import DAG
+# from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
-import yaml
-from datetime import timedelta
+from utils import build_portal_dags
 
-from airflow.operators.dummy_operator import DummyOperator
-from utils import open_config
-
-# dag, task_group = build_portal_dags(filename='config/portal/naver.yaml')
-
-filename='config/portal/naver.yaml'
-config = open_config(filename=filename)
-
-dag = DAG(**config['dag'], default_args={
-    **config['default_args'],
-    'start_date': days_ago(n=1),
-    'retry_delay': timedelta(minutes=10),
-    'execution_timeout': timedelta(hours=1)
-})
-
-task_group = {}
-for item in config['tasks']:
-    name = item['category']
-    if name not in task_group:
-        task_group[name] = []
-
-    task_group[name].append(KubernetesPodOperator(
-        dag=dag,
-        name='task',
-        task_id=item['task_id'],
-        arguments=config['operator']['args'] + [
-            '--config',
-            item['config'],
-            '--sub-category',
-            item['name'],
-        ],
-        **config['operator']['params']
-    ))
-
-start = DummyOperator(task_id='start', dag=dag)
+filename = 'config/portal/naver.yaml'
+dag, task_group = build_portal_dags(filename=filename)
 
 group_list = []
 for name in task_group:
     grp = DummyOperator(task_id=name, dag=dag)
-    grp >> task_group[name]
+    grp.set_downstream(task_or_task_list=task_group[name])
 
     group_list.append(grp)
 
-start >> group_list
+start = DummyOperator(task_id='start', dag=dag)
+start.set_downstream(task_or_task_list=group_list)
