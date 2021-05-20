de58ae32dac69c38fe2b8391d0305b97211db080
ejpark <ejpark78@gmail.com>
2021-05-20 16:25:07 +0900

    change adhoc crawler version

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from glob import glob
-
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
